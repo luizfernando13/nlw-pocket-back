27cb7de0cec3d99d8b842baa2c8fb30c53ddb306
luizfernando13 <43101258+luizfernando13@users.noreply.github.com>
2024-09-16 18:29:56 -0300

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,9 @@
 import z from 'zod'
 
+console.log(process.env)
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   PORT: z.coerce.number(),
 })
 
 export const env = envSchema.parse(process.env)
-console.log(env)
