34bed5fd3fe670b5289fa817edc5a278cfd8a6d2
luizfernando13 <43101258+luizfernando13@users.noreply.github.com>
2024-09-16 16:57:24 -0300

    Update test.js

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 // Pega o diretório atual
 const currentDirectory = __dirname;
+const secretsDirectory = '/etc/secrets'; // Diretório de secrets
 
 function listFiles(directory) {
   // Lê os arquivos e diretórios no diretório atual
@@ -20,16 +21,41 @@ function listFiles(directory) {
           return console.error('Erro ao obter informações do arquivo:', err);
         }
 
+        // Ignora diretórios que contenham "node_modules" ou ".git"
         if (stats.isDirectory()) {
-          // Ignora diretórios que contenham "node_modules"
-          if (file.includes("node_modules")) {
+          if (file.includes("node_modules") || file.includes(".git")) {
             return;
           }
           console.log('Diretório:', filePath);
           // Se for um diretório, chama recursivamente para listar os arquivos dentro
           listFiles(filePath);
         } else {
+          // Caso queira listar arquivos, descomente a linha abaixo:
           // console.log('Arquivo:', filePath);
+        }
+      });
+    });
+  });
+}
+
+function listSecretsDirectory() {
+  fs.readdir(secretsDirectory, (err, files) => {
+    if (err) {
+      return console.error('Erro ao ler o diretório de secrets:', err);
+    }
+
+    console.log(`\nArquivos no diretório /etc/secrets/:`);
+
+    files.forEach(file => {
+      const filePath = path.join(secretsDirectory, file);
+
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          return console.error('Erro ao obter informações do arquivo:', err);
+        }
+
+        if (stats.isFile()) {
+          console.log('Arquivo de Secret:', filePath);
         }
       });
     });
@@ -38,3 +64,6 @@ function listFiles(directory) {
 
 // Chama a função para listar os arquivos no diretório atual
 listFiles(currentDirectory);
+
+// Chama a função para listar arquivos no diretório /etc/secrets/
+listSecretsDirectory();
